47d386b09cc600ddffa638540a2d6876e7eaea70
Alex Becerra Garcia <abecerragarcia04@gmail.com>
2024-10-20 22:14:16 +0200

    Created highscore (does not save)

--- Assets/Scripts/ScoreUI.cs
+++ Assets/Scripts/ScoreUI.cs
@@ -8,8 +8,10 @@ public class ScoreUI : MonoBehaviour
     // Start is called before the first frame update
 
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI bestText;
     public static ScoreUI instance;
     private int score;
+    private int best;
 
     private void Awake()
     {
@@ -21,11 +23,22 @@ public class ScoreUI : MonoBehaviour
     private void Start()
     {
         scoreText.text = score.ToString();
+        bestText.text = best.ToString();
     }
 
     public void UpdateScore()
     {
         score++;
         scoreText.text = score.ToString();
+        UpdateBest();
+    }
+
+    public void UpdateBest()
+    {
+        if (score>best)
+        {
+            best = score;
+        }
+        bestText.text = best.ToString();
     }
 }
