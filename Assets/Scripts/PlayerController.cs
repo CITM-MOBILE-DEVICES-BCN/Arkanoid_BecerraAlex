a6b54d49ac56735d8d2227e9b07bc4d6796076e3
Alex Becerra Garcia <abecerragarcia04@gmail.com>
2024-10-21 23:13:12 +0200

    Playing with scale + added autoPlay

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,18 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed = 10f;  
-    public float boundary = 7.5f;
+    public float boundary = 3.7f;
     public Slider positionSlider; // Referencia al slider.
+    private bool AutoplayEnabled = false;
+    [SerializeField] GameObject ball;
+    private BallController ballController;
+
+
+    void Start()
+    {
+        ballController = ball.GetComponent<BallController>();
+
+    }
 
     void Update()
     {
@@ -23,6 +33,41 @@ public class PlayerController : MonoBehaviour
 
         // Interpola la posición del objeto hacia la nueva posición objetivo.
         transform.position = Vector3.Lerp(transform.position, targetPosition, speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A) || AutoplayEnabled == true)
+        {
+            AutoPlay();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            DisableAutoPlay();
+        }
+       
+       
+    }
+
+    private void AutoPlay()
+    {
+        
+        
+            // Obtén la posición en X de la bola
+            float ballPositionX = ball.transform.position.x;
+
+            // Asegúrate de que el jugador no se salga de los límites establecidos
+            ballPositionX = Mathf.Clamp(ballPositionX, -boundary, boundary);
+
+            // Actualiza la posición del jugador para que siga la posición en X de la bola
+            Vector3 targetPosition = new Vector3(ballPositionX, transform.position.y, transform.position.z);
+
+            // Interpola la posición del jugador hacia la posición objetivo para un movimiento suave
+            transform.position = Vector3.Lerp(transform.position, targetPosition, speed + 10 * Time.deltaTime);
+            AutoplayEnabled = true;
+        
+        
+    }
+
+    private void DisableAutoPlay()
+    {
+        AutoplayEnabled = false;
     }
 }
 
